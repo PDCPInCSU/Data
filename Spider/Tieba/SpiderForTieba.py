81a3e61b62c07178e0f1d909c020d38f1478e3ed
Departures <departures_egoist@icloud.com>
2017-07-02 22:46:03 +0800

    多线程处理帖子内容完成
    多线程处理帖子内容完成，下一步是多线程去完成帖子url链接

--- Spider/Tieba/SpiderForTieba.py
+++ Spider/Tieba/SpiderForTieba.py
@@ -3,7 +3,7 @@ import os
 import urllib
 import urllib2
 import re
-import thread
+import threading
 # 测试用，后边可以删掉，因为肯定抓和分析处理是两个进程的
 from TiebaContentAnalysis import *
 
@@ -11,42 +11,54 @@ from TiebaContentAnalysis import *
 class SpiderForTieba:
 
     def __init__(self, owner, target):
-
         # 爬虫的用户
         self.owner = owner
-        self.target = urllib.quote(target)
-        self.file = None
-        self.contentAnalysis = TiebaContentAnalysis(owner)
+        self.target = target
+        self.contentAnalysis = TiebaContentAnalysis(owner, target)
+        self.mapMutex = threading.RLock()
+        self.map = set()
+        try:
+            if not os.path.exists('../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+target):
+                os.makedirs('../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+target)
+        except IOError as err:
+            print "Error in creating file Path: ../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/"+target
+            print "Error is " + str(err)
 
     def openURL(self, url):
-
         request = urllib2.Request(url)
         response = urllib2.urlopen(request)
-
         return response.read()
 
     # def pageController(self):
 
-    def getIndex(self, pageNum):
-
-        url = "http://tieba.baidu.com/f?kw=" + self.target + "ie=utf-8&pn=" + str(50 * pageNum-1)
-        content = self.openURL(url)
-        patternForIndex = '(?:="/)(?:/tieba\.baidu\.com)?/p/(\d+)'
+    def getIndex(self, pageNum, ):
+        url = "http://tieba.baidu.com/f?kw=" + urllib.quote(self.target) + "ie=utf-8&pn=" + str(50 * pageNum-1)
+        try:
+            content = self.openURL(url)
+        except IOError as err:
+            print "Error in opening url : "+ url
+            # 不知道该Return什么0.0 其实应该是这个直接结束的
+            return
+        patternForIndex = '=\"/p/(\d+)'
         result = re.findall(patternForIndex, content)
-
-
+        self.mapMutex.acquire()
+        for count in range(0,len(result)):
+            # print result[count] # 用于调试self.map
+            if result[count] not in self.map:
+                self.map.add(result[count])
+                t = threading.Thread(target=self.getThreadContent, args=(result[count],))
+                t.start()
+        self.mapMutex.release()
+        # print len(result)# 用于调试self.map
 
     def getThreadContent(self, urlString):
-
-        pageNum = 1
-
         try:
             url = 'http://tieba.baidu.com/p/' + urlString
             content = self.openURL(url)
             # 获取页面数
             pattern = re.compile('pn=(?P<pageNum>\d)">尾页</a>')
             pageNumResult = re.search(pattern, content)
-            if pageNumResult != None:
+            if pageNumResult is not None:
                 pageNum = int(pageNumResult.group('pageNum'))
             else:
                 pageNum = 1
@@ -61,12 +73,11 @@ class SpiderForTieba:
                 count += 1
         except IOError as err :
             print "Error in opening Tieba " + urlString
-        # print self.contentAnalysis.readCacheFile(urlString)
+            print "Error is " + str(err)
 
     # 将相应的爬下来的内容写入Cache文件
     def createCacheFile(self, name, content):
-
-        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'
+        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+self.target + '/'
         filePath = dirPath + name
         try:
             if not os.path.exists(dirPath):
@@ -74,8 +85,8 @@ class SpiderForTieba:
             cacheFile = open(filePath, 'a+')
             cacheFile.writelines(content)
         except IOError as err:
-            print "Error is " + err
-            print "Error in Creating or writing into Data/Cache/SpiderCache/TiebaCache/Analysing Cache/" + name
+            print "Error is " + str(err)
+            print "Error in Creating or writing into Data/Cache/SpiderCache/TiebaCache/Analysing Cache/"+self.target + '/' +name
         finally:
             if 'cacheFile' in locals():
                 cacheFile.close()
@@ -84,4 +95,5 @@ class SpiderForTieba:
 
 
 test = SpiderForTieba(0, '中南大学')
-test.getThreadContent('5181822218')
+test.getIndex(1)
+# test.getThreadContent('5181822218')
--- Spider/Tieba/TiebaContentAnalysis.py
+++ Spider/Tieba/TiebaContentAnalysis.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
-
 class TiebaContentAnalysis:
 
-    def __init__(self, owner):
+    def __init__(self, owner, target):
         self.owner = owner
+        self.target = target
+
 
     # 读取文件，但是如果打开文件失败会返回None
     def readCacheFile(self, name):
-        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'
+        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+self.target
         filePath = dirPath + name
         result = None
         try:
             cacheFile = open(filePath, 'r')
             result = cacheFile.read()
         except IOError as err:
-            print "Error in Reading file Data/Cache/SpiderCache/TiebaCache/Analysing Cache/" + name
+            print "Error in Reading file Data/Cache/SpiderCache/TiebaCache/Analysing Cache/" +self.target+ name
             print "Error is " + err
         finally:
             if 'cacheFile' in locals():
