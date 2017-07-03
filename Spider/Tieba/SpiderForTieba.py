488d390792c4c50abf15c7e20c454deea5e6677b
Departures <departures_egoist@icloud.com>
2017-07-03 21:30:14 +0800

    爬虫基础功能完成
    爬虫基础功能完成，可以多线程爬了，但是还需要写一个控制函数来和taskManager交互，同时，我把该改成私有的都改了

--- Spider/Tieba/SpiderForTieba.py
+++ Spider/Tieba/SpiderForTieba.py
@@ -5,21 +5,24 @@ import urllib2
 import re
 import threading
 import time
+import requests
 # 测试用，后边可以删掉，因为肯定抓和分析处理是两个进程的
 from TiebaContentAnalysis import *
 
 
 class SpiderForTieba:
 
-    def __init__(self, owner, target):
+    def __init__(self, owner, target, timer, pageRange):
         # 爬虫的用户
-        self.owner = owner
-        self.target = target
-        self.contentAnalysis = TiebaContentAnalysis(owner, target)
-        self.mapMutex = threading.RLock()
-        self.map = [set(''), set(''), set('')]
-        self.testnum = 0
-        # self.pageControllerFlag = False
+        self.__owner = owner
+        self.__target = target
+        self.__contentAnalysis = TiebaContentAnalysis(owner, target)
+        self.__mapMutex = threading.RLock()
+        self.__map = [set(''), set(''), set('')]
+        self.__headers = None
+        self.__cookieTimeOut = True
+        self.__timer = timer
+        self.__pageRange = pageRange
         try:
             if not os.path.exists('../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+target):
                 os.makedirs('../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+target)
@@ -27,89 +30,93 @@ class SpiderForTieba:
             print "Error in creating file Path: ../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/"+target
             print "Error is " + str(err)
 
-    def openURL(self, url):
-        flag = True
-        request = urllib2.Request(url)
-        while flag:
-            try:
-                response = urllib2.urlopen(request)
-                flag = False
-            except IOError:
-                pass
-        return response.read()
+        self.__pageController(self.__pageRange, timer)
 
-    def pageController(self, pageRange, timer):
+    def __pageController(self, pageRange, timer):
         setNum = 0
-        test = True
-        while test:
+        while True:
             for count in range(1, pageRange+1):
-                t = threading.Thread(target=self.getIndex, args=(count, setNum%3))
+                t = threading.Thread(target=self.__getIndex, args=(count, setNum%3))
                 t.start()
             setNum += 1
             time.sleep(timer)
-            print "test"
-            test = False
+
+
+    def __openURL(self, url):
+        flag = True
+        response = None
+        if self.__cookieTimeOut:
+            conn = requests.session()
+            resp = conn.get(url)
+            self.__headers = resp.request.headers
+            self.__headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
+            self.__headers['Accept-Encoding'] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+            self.cookieTimeOut = False
+        while flag:
+            try:
+                request = urllib2.Request(url, headers=self.__headers)
+                response = urllib2.urlopen(request)
+                flag = False
+            except IOError:
+                print "Error in opening URL "+ url
+                pass
+        return response.read()
 
 
     # 对相应的首页页面
-    def getIndex(self, pageNum, setNum):
-        # print pageNum
-        url = "http://tieba.baidu.com/f?kw=" + urllib.quote(self.target) + "&ie=utf-8&pn=" + str(50 * (pageNum-1))
+    def __getIndex(self, pageNum, setNum):
+        url = "http://tieba.baidu.com/f?kw=" + urllib.quote(self.__target) + "&ie=utf-8&pn=" + str(50 * (pageNum-1))
+        print url
         try:
-            content = self.openURL(url)
+            content = self.__openURL(url)
         except IOError as err:
             print "Error in opening url : "+ url
             # 不知道该Return什么0.0 其实应该是这个直接结束的
             return
         patternForIndex = '=\"/p/(\d+)'
         result = re.findall(patternForIndex, content)
-
-        self.mapMutex.acquire()
+        self.__mapMutex.acquire()
         for count in range(0,len(result)):
-            # print result[count] # 用于调试self.map
-            # print self.map[setNum]
-            if result[count] not in self.map[setNum] :# 注意，正在修改
+
+            if result[count] not in self.__map[setNum] :# 注意，正在修改
                 # self.testnum += 1
                 # print self.testnum
-                self.map[setNum].add(result[count])
-                t = threading.Thread(target=self.getThreadContent, args=(result[count],))
+                self.__map[setNum].add(result[count])
+                t = threading.Thread(target=self.__getThreadContent, args=(result[count],))
                 t.start()
-        self.mapMutex.release()
-        # print len(self.map[setNum])
+        self.__mapMutex.release()
+        print len(self.__map[setNum])
 
-        # print len(result)# 用于调试self.map
+        # print len(result)# 用于调试self.__map
 
-    def getThreadContent(self, urlString):
-        # self.mapMutex.acquire()
-        # self.testnum += 1
-        # print self.testnum
-        # self.mapMutex.release()
+    def __getThreadContent(self, urlString):
         try:
             url = 'http://tieba.baidu.com/p/' + urlString
-            content = self.openURL(url)
+            content = self.__openURL(url)
             # 获取页面数
-            pattern = re.compile('pn=(?P<pageNum>\d)">尾页</a>')
+            pattern = re.compile('pn=(?P<pageNum>\d+)">尾页</a>')
             pageNumResult = re.search(pattern, content)
             if pageNumResult is not None:
                 pageNum = int(pageNumResult.group('pageNum'))
             else:
                 pageNum = 1
-
-            self.createCacheFile(urlString, content)
+            self.__createCacheFile(urlString, content)
             # 对于多页面帖子：
             count = 1
             while count != pageNum:
-                url = 'http://tieba.baidu.com/p/' + urlString + '?pn=' + str(count)
-                content = self.openURL(url)
-                self.createCacheFile(urlString, content)
                 count += 1
+                url = 'http://tieba.baidu.com/p/' + urlString + '?pn=' + str(count)
+                content = self.__openURL(url)
+                self.__createCacheFile(urlString, content)
+
+
         except IOError as err :
             print "Error in opening Tieba " + urlString
             print "Error is " + str(err)
 
     # 将相应的爬下来的内容写入Cache文件
-    def createCacheFile(self, name, content):
-        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+self.target + '/'
+    def __createCacheFile(self, name, content):
+        dirPath = '../Data/Cache/SpiderCache/TiebaCache/Analysing Cache/'+self.__target + '/'
         filePath = dirPath + name
         try:
             if not os.path.exists(dirPath):
@@ -118,7 +125,7 @@ class SpiderForTieba:
             cacheFile.writelines(content)
         except IOError as err:
             print "Error is " + str(err)
-            print "Error in Creating or writing into Data/Cache/SpiderCache/TiebaCache/Analysing Cache/"+self.target + '/' +name
+            print "Error in Creating or writing into Data/Cache/SpiderCache/TiebaCache/Analysing Cache/"+self.__target + '/' +name
         finally:
             if 'cacheFile' in locals():
                 cacheFile.close()
@@ -126,7 +133,8 @@ class SpiderForTieba:
 
 
 
-test = SpiderForTieba(0, '刀剑神域')
-test.pageController(3, 5)
+test = SpiderForTieba(owner=0, target='刀剑神域', timer=5, pageRange=2)
+
 # test.getIndex(1)
-# test.getThreadContent('5181822218')
+# test.getThreadContent('689776012')
+# print test.openURL("https://tieba.baidu.com/p/689776012")
